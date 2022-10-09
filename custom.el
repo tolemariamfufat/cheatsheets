0fd0cd3df27f3331c24fff01361515934cf5b879
Tolemariam Fufa Teso <99896370+tolemariamfufat@users.noreply.github.com>
2022-10-09 11:42:41 +0300

    Create custom.el

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-vivendi))
+ '(custom-safe-themes
+   '("3fe1ebb870cc8a28e69763dde7b08c0f6b7e71cc310ffc3394622e5df6e4f0da" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "eca44f32ae038d7a50ce9c00693b8986f4ab625d5f2b4485e20f22c47f2634ae" "e87f48ec4aebdca07bb865b90088eb28ae4b286ee8473aadb39213d361d0c45f" default))
+ '(helm-minibuffer-history-key "M-p")
+ '(package-selected-packages
+   '(hide-mode-line org-tree-slide org-roam counsel-ag-popup org-modern counsel-at-point ivs-edit ivy-avy ivy-clipmenu ivy-clojuredocs ivy-dired-history ivy-emms ivy-emoji ivy-erlang-complete ivy-explorer ivy-file-preview ivy-fuz ivy-gitlab ivy-historian ivy-hydra ivy-lobsters ivy-migemo ivy-mpdel ivy-omni-org ivy-pass ivy-phpunit ivy-posframe ivy-prescient ivy-purpose ivy-rich ivy-rtags ivy-searcher ivy-spotify ivy-todo ivy-xcdoc ivy-xref ivy-yasnippet ivy-ycmd ivy-youtube swiper-helm swiper modus-themes doom-modeline ivy command-log-mode use-package))
+ '(warning-suppress-types '((use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
